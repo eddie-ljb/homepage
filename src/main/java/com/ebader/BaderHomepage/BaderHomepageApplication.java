702d66e7314224fc86f90aa0e1ff500c878fa7b9
eddie-ljb <69401607+eddie-ljb@users.noreply.github.com>
2024-05-14 14:00:56 +0200

    initial commit

---
+++ src/main/java/com/ebader/BaderHomepage/BaderHomepageApplication.java
@@ -0,0 +1,13 @@
+package com.ebader.BaderHomepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaderHomepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaderHomepageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ebader/BaderHomepage/BaderHomepageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebader.BaderHomepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaderHomepageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
